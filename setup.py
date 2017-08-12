42e32beaf7882d71028ecb8539f2c8bc7d9c2cea
Philipp Schmitt <philipp@schmitt.co>
2017-08-12 21:30:43 +0200

    Version 1.1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(fname):
 
 setup(
     name='pynello',
-    version='1.0-1',
+    version='1.1',
     license='GPL3',
     description='Python library for nello.io intercoms',
     long_description=read('README.rst'),
